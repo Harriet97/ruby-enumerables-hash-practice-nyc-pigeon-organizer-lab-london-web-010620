4079b831eb79fdb93a9e437d047512757673ba4d
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-29 10:21:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each do |info, value|
     value.each do |stats, names|
       names.each do |name|
+        if pigeon_list[name] == nil
   
   end
   pigeon_list
